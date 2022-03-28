9d6b8ab29f7024340161f3b32973aa3d67018d5e
Makai Pan <makai820117@gmail.com>
2022-03-29 00:23:32 +0800

    Complete Post Bloc

--- lib/post/bloc/post_bloc.dart
+++ lib/post/bloc/post_bloc.dart
@@ -1,15 +1,28 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+
+import '../model/post.dart';
+import '../post_repository.dart';
+import '../service/http_service.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
+  final PostRepository repository = PostRepository();
+
   PostBloc() : super(PostInitial()) {
-    on<PostEvent>((event, emit) {
-      // TODO: implement event handler
+    on<PostFetch>((event, emit) async {
+      if (state is PostFetching || state is PostReachedBottom) return;
+      if (state is! PostInitial) emit(PostFetching());
+      List<Post> newPosts =
+          await HttpService.fetchPosts(repository.posts.length);
+      if (newPosts.isNotEmpty) {
+        repository.posts.addAll(newPosts);
+        emit(PostFetchedDone());
+      } else {
+        emit(PostReachedBottom());
+      }
     });
   }
 }
